75d3a47fcfd5e34dce7504e35a06642268bedf9b
hiroshi.miyata <n.r.n.1.814@gmail.com>
2020-08-30 04:13:26 +0000

    add pagination

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+  root to: 'tasks#index'
+  
   get 'toppages/index'
   get 'users/index'
   get 'users/show'
   get 'users/new'
   get 'users/create'
-  root to: 'tasks#index'
+  
   
   get 'signup', to: 'users#new'
   resources :users, only: [:index, :show, :new, :create]
