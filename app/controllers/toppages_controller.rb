75195cf6f57a13a76878d7eec03013e7acb6110b
hiroshi.miyata <n.r.n.1.814@gmail.com>
2020-08-30 03:26:02 +0000

    set preparation

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -1,4 +1,8 @@
 class ToppagesController < ApplicationController
   def index
+    if logged_in?
+      @kadai-tasklist = current_user.kadai-tasklist.build  # form_with 用
+      @kadai-tasklist = current_user.kadai-tasklist.order(id: :desc).page(params[:page])
+    end
   end
 end
