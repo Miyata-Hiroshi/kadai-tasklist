9d5a69fbaeeb22c947cd92691626f47f85a621df
hiroshi.miyata <n.r.n.1.814@gmail.com>
2020-09-10 21:31:36 +0000

    Lesson15

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-    
+
   include SessionsHelper
 
   private
@@ -9,4 +9,9 @@ class ApplicationController < ActionController::Base
       redirect_to login_url
     end
   end
+
+  def counts(user)
+    @count_tasks = user.tasks.count
+  end
+  
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,8 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @tasks = @user.tasks.order(id: :desc).page(params[:page])
+    counts(@user)
   end
 
   def new
@@ -14,21 +16,14 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)
-
-    if @user.save
-      flash[:success] = 'ユーザを登録しました。'
-      redirect_to @user
-    else
-      flash.now[:danger] = 'ユーザの登録に失敗しました。'
-      render :new
-    end
+   @user = User.new(user_params)  
+  
   end
 
   private
 
-  def user_params
+ def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
-  end
+ end
   
 end
