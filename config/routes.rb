86b2166874b73fb62d1f8b500e39eb24095a52a2
hiroshi.miyata <n.r.n.1.814@gmail.com>
2020-09-13 14:53:22 +0000

    fix4

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
   root to: 'tasks#index'
   
-  get 'tasks/:id/edit', to: 'tasks#edit'
+
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
   
   get 'signup', to: 'users#new'
-  resources :users, only: [:index, :show, :new, :create]
   
-  resources :tasks
+  resources :users, only: [:index, :show, :new, :create]
+  resources :tasks,only: [:index, :show, :new, :create, :destroy]
   
 end
